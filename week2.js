08a7f0df532b3572450760cabfbfb1711689b2e7
unknown <B022010020@student.utem.edu.my>
2022-03-16 21:39:59 +0800

    first commit

---
+++ week2.js
@@ -0,0 +1,22 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:Tyx666288%40@cluster0.lzh3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  if (err) {
+    console.log(err.message)
+    return
+}
+  console.log('Database connected');
+
+client.db().admin().listDatabases().then(result => {
+       console.log(result['databases'][7]);
+
+client.db('sample_training').collection('trips').find({'birth year':1951}).toArray().then(result =>{
+  console.log(result);
+});             
+// perform actions on the collection object
+});
+});
+
+client.close();
+
